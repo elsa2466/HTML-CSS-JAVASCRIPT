fd63c2ad7e3b79a93bac2e6b66cafd7cca922082
elsa2466 <elsa.sherifi09@gmail.com>
2025-03-26 12:44:31 -0700

    moduli26

---
+++ moduli26/main.js
@@ -0,0 +1,27 @@
+console.log("Hello");
+
+var a= 50+50;
+console.log(a);
+
+var b=30;
+var c =20;
+var d= b+c;
+console.log(d);
+
+var h=30;
+var g =15;
+var f=h-g;
+console.log(f);
+
+var k=60;
+var t =10;
+var rezultati=k/t;
+console.log(rezultati);
+
+var p = 100;
+p++;
+console.log(p);
+
+var o = 100;
+o--;
+console.log(o);
