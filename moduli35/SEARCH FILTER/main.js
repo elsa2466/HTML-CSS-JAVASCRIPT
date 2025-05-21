bfcf2e5cbb58d3bb33178a0bedbf3810c450cd75
elsa2466 <elsa.sherifi09@gmail.com>
2025-05-21 11:48:56 -0700

    moduli35

---
+++ moduli35/SEARCH FILTER/main.js
@@ -0,0 +1,15 @@
+function search(){
+    var input,filter,ul,li,a,i,textValue;
+
+}
+
+input=document.getElementById("myInput");
+
+filter = input.value.toUpperCase();
+
+ul=document.getElementById("myList");
+
+for (i=0; i<li.length;i++){
+    a=li[i];
+
+}
