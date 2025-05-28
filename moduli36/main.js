99b465834b4edf7f44d88958308f9fb354b1fb73
elsa2466 <elsa.sherifi09@gmail.com>
2025-05-28 11:44:09 -0700

    moduli37

--- moduli36/main.js
+++ moduli36/main.js
@@ -18,4 +18,20 @@ $('#btn').click(function(params) {
     console.log($('#h1').text());
     $('#h1').text('testing text');
      $('#h1').append('extra text');
-})
+});
+
+$("#btn1").click(function(){
+    $(".hidden").show('slow');
+});
+
+$("#btn2").click(function(){
+    $(".hidden").hide();
+});
+
+$('#square').click(function(){
+$('#square').animate({
+
+    'width':'200px',
+    'height':'200px',
+});
+});
